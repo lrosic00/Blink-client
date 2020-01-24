d88a91c16dff7eee3c9a4155984a2e8654c1e835
Luka Rosic <lrosic00@fesb.hr>
2020-01-24 17:49:44 +0100

    trying to deploy...

--- src/redux/store.js
+++ src/redux/store.js
@@ -32,7 +32,7 @@ const store = createStore(
 		window.navigator.userAgent.includes("Chrome")
 			? window.__REDUX_DEVTOOLS_EXTENSION__ &&
 					window.__REDUX_DEVTOOLS_EXTENSION__()
-			: compose
+			: a => a
 	)
 );
 
